774dc3eb90221d9ff0d04b51bc17477de6a15b31
AhmedTrabelsi2000 <trabelsi.ahmed.pro@gmail.com>
2023-08-24 13:31:49 +0100

    Add build folder for deployment

---
+++ build/precache-manifest.4d43fdd4feb8b84ad17c13af149536f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801dbe8da44d0e12e47497e52deb0fdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69772e56a36ca7eb3644",
+    "url": "/static/css/2.41bc09fa.chunk.css"
+  },
+  {
+    "revision": "0784fbe71b681edd06d8",
+    "url": "/static/css/main.3ce5da03.chunk.css"
+  },
+  {
+    "revision": "69772e56a36ca7eb3644",
+    "url": "/static/js/2.a6c50e8f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a6c50e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0784fbe71b681edd06d8",
+    "url": "/static/js/main.ba59efc8.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
